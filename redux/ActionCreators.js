08dba9f0ac887bbaa5ab75463e4ce758573420bc
Gabriel <gbschram@utica.edu>
2020-11-30 12:39:13 -0500

    Favorites Component, Swipe Options, and Alert Added

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -169,7 +169,10 @@ export const addComment= comment => ({
     type: ActionTypes.ADD_COMMENT,
     payload: comment
 })
-
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
 
 
 
